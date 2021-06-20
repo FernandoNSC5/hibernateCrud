d07840bdd862260be132e754db9233075bad14a9
FernandoNSC5 <fernandonsc5@gmail.com>
2021-06-20 10:50:22 -0300

    initial commit

---
+++ src/test/java/br/com/bluemoth/HibernateCrud/HibernateCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bluemoth.HibernateCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
