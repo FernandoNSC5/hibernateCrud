3a516cf82bb9dec21c452adfcf86a57b0a0e75d3
FernandoNSC5 <fernandonsc5@gmail.com>
2021-06-20 11:27:40 -0300

    Alternate web-services port to 8080

--- src/main/java/br/com/bluemoth/HibernateCrud/HibernateCrudApplication.java
+++ src/main/java/br/com/bluemoth/HibernateCrud/HibernateCrudApplication.java
@@ -1,5 +1,7 @@
 package br.com.bluemoth.HibernateCrud;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HibernateCrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HibernateCrudApplication.class, args);
+		SpringApplication app = new SpringApplication(HibernateCrudApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8080"));
+        app.run(args);
 	}
 
 }
